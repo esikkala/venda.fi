6dbb9ee08a94ff0a7bf572af3c23e9331c3bb586
eskoik <esko.ikkala@crasman.fi>
2022-08-21 07:40:53 +0300

    Use link to load roboto font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,9 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="robots" content="noindex" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -1,17 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
-
   body {
     margin: 0;
     min-width: 1680px;
     overflow-x: auto;
     font-family: 'Roboto', sans-serif;
   }
-
- 
 `;
 
 export default GlobalStyle;
