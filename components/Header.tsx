0db549c9873f9fb6f5a5853ce3ba8566a5f1e85f
eskoik <esko.ikkala@crasman.fi>
2022-09-04 10:01:41 +0300

    Active style for nav links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react';
-import { useRouter } from 'next/router';
+import React, { useContext, useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { I18nContext } from '../contexts/I18nContext';
 // import vendaLogo from '../public/venda_logo.svg';
@@ -23,30 +22,28 @@ export const HeaderBackground = styled.div`
   background-color: rgba(255, 255, 255, 0.5);
 `;
 
-const LinkStyle = css`
+const LinkColor = css`
   color: ${({ theme }) => theme.colors.gray};
 `;
 
-const ActiveLinkStyle = css`
+const LinkColorActive = css`
   color: ${({ theme }) => theme.colors.orange};
 `;
 
-type LinkContentProps = {
-  isActive: boolean
-};
-
 // https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-custom-component-that-wraps-an-a-tag
-const LinkContent = styled.a<LinkContentProps>`
+const LinkContent = styled.a<{ pageId: string, activePage: string }>`
   font-family: 'Helvetica', 'Arial', sans-serif;
   font-weight: 600;
   font-size: 17px;
   letter-spacing: 1px;
- 
   text-decoration: unset;
-  ${({ isActive }) => {
-    if (isActive) return ActiveLinkStyle;
-    if (!isActive) return LinkStyle;
-    return null;
+  
+    
+  ${({ pageId, activePage }) => {
+    if (pageId === activePage) {
+      return LinkColorActive;
+    }
+    return LinkColor;
   }}
 `;
 
@@ -64,27 +61,57 @@ const LogoContainer = styled.a`
 function Header() {
   const { localizations } = useContext(I18nContext);
   const { header } = localizations;
-  const router = useRouter();
-  const { pathname } = router;
+  const [activePage, setActivePage] = useState('#home');
+  console.log(activePage);
+
+  const handleScroll = () => {
+    setActivePage(window.location.hash);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
   return (
     <Nav>
       <LogoContainer href="#home">
         {/* eslint-disable @next/next/no-img-element */}
         <img src="venda_logo.svg" width={111} height={144} alt="Venda logo" />
       </LogoContainer>
-      <LinkContent href="#marketing-communications" isActive={pathname.includes('#marketing-communications')}>
+      <LinkContent
+        href="#marketing-communications"
+        pageId="#marketing-communications"
+        activePage={activePage}
+      >
         {header.marketingCommmunications}
       </LinkContent>
-      <LinkContent href="#translation-services" isActive={pathname.includes('#translation-services')}>
+      <LinkContent
+        href="#translation-services"
+        pageId="#translation-services"
+        activePage={activePage}
+      >
         {header.translations}
       </LinkContent>
-      <LinkContent href="#outsource-your-project" isActive={pathname.includes('##outsource-your-project')}>
+      <LinkContent
+        href="#outsource-your-project"
+        pageId="#outsource-your-project"
+        activePage={activePage}
+      >
         {header.outsourceYourProject}
       </LinkContent>
-      <LinkContent href="#briefly" isActive={pathname.includes('#contact')}>
+      <LinkContent
+        href="#briefly"
+        pageId="#briefly"
+        activePage={activePage}
+      >
         {header.briefly}
       </LinkContent>
-      <LinkContent href="#contact" isActive={pathname.includes('#contact')}>
+      <LinkContent
+        href="#contact"
+        pageId="#contact"
+        activePage={activePage}
+      >
         {header.contact}
       </LinkContent>
     </Nav>
--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -2,11 +2,15 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   html {
+  
+    overflow: hidden;
+    
     scroll-behavior: smooth;
     @media screen and (prefers-reduced-motion: reduce) {
       scroll-behavior: auto;
     }
   }
+
   body {
     margin: 0;
     min-width: 1550px;
