7f7bad39020bc0808cc8b7426703e03700dd8989
eskoik <esko.ikkala@crasman.fi>
2022-09-04 13:08:28 +0300

    Build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-  basePath: '/venda.fi',
+  assetPrefix: isProd ? 'venda.fi' : undefined,
 }
 
 module.exports = nextConfig
